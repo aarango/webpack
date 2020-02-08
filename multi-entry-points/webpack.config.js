a37ac2fb51dd100addd88670afe1c0f40e3d81e1
Andres B <ardobotcomercial@gmail.com>
2020-02-08 11:44:49 -0500

    web_pack

---
+++ multi-entry-points/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+    home: path.resolve(__dirname, 'src/js/index.js'),
+    precios: path.resolve(__dirname, 'src/js/precios.js'),
+    contact: path.resolve(__dirname, 'src/js/contact.js')
+    },
+    mode:'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'  // corresponde a los nombre de los path home, precios, etx
+    }
+
+
+    
+}
