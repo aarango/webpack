33c6d3c76d94df49d010dabbe8c6a29a11f26fb6
Andres B <ardobotcomercial@gmail.com>
2020-02-11 17:20:16 -0500

    Add JSX - WEBPACK

--- react/src/js/index.js
+++ react/src/js/index.js
@@ -1,13 +1,5 @@
-import "../css/index.css";
-import search from "./search.js.js";
-import render from "./render.js.js";
-
-const id = prompt("quien es el pokemos");
-
-search(id)
-  .then(data => {
-    render(data);
-  })
-  .catch(() => {
-    console.log("no hubo pokemos");
-  });
+import '../css/index.css'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './component/App'
+render(<App />, document.getElementById('container'))
